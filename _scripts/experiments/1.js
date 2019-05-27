ce52d7e7cbc3b2ea803212eff9307e1089821a5c
Talor Anderson <ta.anders17@gmail.com>
2019-05-27 14:57:55 -0700

    finish animation 1

--- _scripts/experiments/1.js
+++ _scripts/experiments/1.js
@@ -2,54 +2,103 @@
 import anime from 'animejs'
 import SimplexNoise from 'simplex-noise';
 
-// console.log(anime)
+export const createDotGrid = (selector, grid, delay, duration) => {
+  let totalAnimationTime = delay + duration
+  let noise = []
+  let simplex = new SimplexNoise(Math.random);
+  let numberOfElements = grid[0] * grid[1];
 
-export const createDotGrid = (selector) => {
-  var testArea = document.querySelector(selector);
-  testArea.innerHTML = ""
 
-  var shadowWrapperEl = document.createElement('div');
+  //--------- Create Wrapper elements
+  let wrapper = document.querySelector(selector);
+  wrapper.innerHTML = ""
+
+  let shadowWrapperEl = document.createElement('div');
   shadowWrapperEl.className = "dots-shadow-wrapper"
 
-  var dotsWrapperEl = document.createElement('div');
+  let dotsWrapperEl = document.createElement('div');
   dotsWrapperEl.className = "dots-wrapper"
 
-  testArea.appendChild(shadowWrapperEl)
-  testArea.appendChild(dotsWrapperEl)
+  wrapper.appendChild(shadowWrapperEl)
+  wrapper.appendChild(dotsWrapperEl)
+
+  let dotsFragment = document.createDocumentFragment();
+  let shadowFragment = document.createDocumentFragment();
   
-  
-  var dotsFragment = document.createDocumentFragment();
-  var shadowFragment = document.createDocumentFragment();
-  var grid = [10, 8];
-  var numberOfElements = grid[0] * grid[1];
-  var noise = []
 
-  var delay = 2000;
-  var duration = 2000;
 
-  var totalAnimationTime = delay + duration
-
-  var simplex = new SimplexNoise(Math.random);
-
-  for (var i = 0; i < grid[0]; i++) {
-    for (var j = 0; j < grid[1]; j++) {
-      var noiseVal = simplex.noise2D(i, j) * totalAnimationTime + totalAnimationTime
+  //--------- Fill the grid  
+  for (let i = 0; i < grid[0]; i++) {
+    for (let j = 0; j < grid[1]; j++) {
+      
+      //this will be used as the delay to start the animation
+      let noiseVal = simplex.noise2D(i, j) * totalAnimationTime + totalAnimationTime
       noise.push(Math.round(noiseVal));
-      var dotEl = document.createElement('div');
+
+      //create the dot element and corresponding shadow
+      let dotEl = document.createElement('div');
       dotEl.classList.add('dot');
-      var shadowEl = document.createElement('div');
+      let shadowEl = document.createElement('div');
       shadowEl.classList.add('dot-shadow');
+
+      //append dot and shadow to their containers
       dotsFragment.appendChild(dotEl);
       shadowFragment.appendChild(shadowEl);
     }
   }
-  console.log(noise)
 
+  //add the dot and shadow containers to the page
   dotsWrapperEl.appendChild(dotsFragment);
   shadowWrapperEl.appendChild(shadowFragment);
 
-  for (var i = 0; i < numberOfElements; i++) {
-    var anim1 = anime({
+  //--------- Create controller buttons
+  const controls = document.createElement("div")
+  controls.className = "controls"
+  
+  const colors = [
+    "#3837D6",
+    "#502772",
+    "#732752",
+    "#257243",
+  ]
+  //wrapper.style.color will determine the color of all the dots
+  wrapper.style.color = colors[0]
+
+  colors.forEach((color) => {
+
+    //create button
+    const button = document.createElement("span");
+    button.style.backgroundColor = color;
+    button.className = "circle-button ";
+
+    // when we click it, animate the dot color and the 'jumping' button
+    button.onclick = () => {
+      anime({
+        targets: ['.dot', '.dot-shadow'],
+        color: color,
+        delay: anime.stagger(100, {
+          grid: grid,
+          from: 'center'
+        })
+      }).play()
+
+      anime({
+        targets: button,
+        translateY: -8,
+        direction: 'alternate',
+        duration: 100,
+        easing: 'easeInOutQuad'
+      })
+    }
+    // add the button to our controller container
+    controls.appendChild(button)
+  })
+  // add the controls to the page
+  wrapper.appendChild(controls)
+
+  // start each dot's animation after a random amount of time
+  for (let i = 0; i < numberOfElements; i++) {
+    let anim1 = anime({
       targets: Array.from(dotsWrapperEl.children)[i],
       translateY: [0, -5, -5, 0],
       translateX: [0, -5, -5, 0],
@@ -59,7 +108,7 @@ export const createDotGrid = (selector) => {
       autoplay: false,
       easing: 'easeInOutSine'
     });
-    var anim2 = anime({
+    let anim2 = anime({
       targets: Array.from(shadowWrapperEl.children)[i],
       translateY: [0, 5, 5, 0],
       translateX: [0, 5, 5, 0],
@@ -69,32 +118,17 @@ export const createDotGrid = (selector) => {
       autoplay: false,
       easing: 'easeInOutSine'
     });
-    console.log('done')
     setTimeout(anim1.play, noise[i])
     setTimeout(anim2.play, noise[i])
 
   }
-  return testArea
+
+  return wrapper
 }
 
-const dotGrid = createDotGrid('.experiment1')
+let grid = [10, 8]; // grid size (x,y)
+let delay = 1200;   // amount of time the dot and shadow will spend 'together'
+let duration = 2000; 
 
-const colors = [
-  "#3837D6",
-  "#502772",
-"#732752",
-"#257243",  
-]
-dotGrid.style.color = colors[0]
-const controls = document.createElement("div")
-controls.className="controls"
+const dotGrid = createDotGrid('.experiment1', grid, delay, duration)
 
-colors.forEach((color)=> {
-  const button = document.createElement("span");
-  button.style.backgroundColor = color;
-  button.className = "circle-button ";
-  button.onclick = ()=> dotGrid.style.color = color;
-  controls.appendChild(button)
-})
-
-dotGrid.appendChild(controls)
